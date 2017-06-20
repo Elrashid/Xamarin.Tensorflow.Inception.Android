b76686cccfb54839798fff2c8fa8790e14de7db2
Elrashid <osman.elrashid@gmail.com>
2017-06-20 17:29:20 +0400

    Add project files.

---
+++ co.elrashid.xam.tf.Incp.and/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace co.elrashid.xam.tf.Incp.and
+{
+    [Activity(Label = "co.elrashid.xam.tf.Incp.and", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
